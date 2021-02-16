4ec57fdc1f8cbb61c08e40fbdab60aefbfd06fd2
Suzana Suzuki <s.dasilvasuzuki@gmail.com>
2021-02-16 15:03:33 -0300

    segundaalterações

---
+++ script.js
@@ -0,0 +1,9 @@
+let B7Validator = {
+    handleSubmit: (event) => {
+        event.preventDefault();
+    }
+};
+ 
+
+let form = document.querySelector('.b7validator');
+form.addEventListener('submit', B7Validator.handleSubmit);
