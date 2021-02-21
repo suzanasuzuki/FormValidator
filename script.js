f631a61086cca294527d2fcc8fbadfe6d806c237
Suzana Suzuki <s.dasilvasuzuki@gmail.com>
2021-02-21 18:44:34 -0300

    maisalteracoes

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 let B7Validator = {
-    handleSubmit: (event) => {
+    handleSubmit:(event) => {
         event.preventDefault();
+        let send  = true;
+
+        let inputs = form.querySelectorAll('input');
+
+        for(let i=0;i<inputs.length;i++) {
+            let input = inputs[i];
+            let check = B7Validator.checkInput(input);
+            if(check !== true) {
+                send = false;
+                // exibir o erro
+            }
+        }
+
+        send = false;
+        if(send) {
+            form.submit();
+        }
+    },
+
+    checkInput:(input) => {
+        let rules = input.getAttribute('data-rules');
     }
-};
+}
  
 
 let form = document.querySelector('.b7validator');
